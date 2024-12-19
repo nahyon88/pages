022a167f95293f958af7271c4ccfe07e9f7f64ed
nahyeon.ryu <nahyeon.ryu@navercorp.com>
2024-12-19 11:12:35 +0900

    v3

--- script.js
+++ script.js
@@ -8,23 +8,20 @@ document.addEventListener("DOMContentLoaded", function() {
     function updateLinePosition() {
         const now = new Date();
         const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-        const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
-        const totalDayTime = endOfDay - startOfDay;
-        const elapsedTime = now - startOfDay;
-        const imgHeight = img.clientHeight;
-        const newYPosition = (elapsedTime / totalDayTime) * imgHeight;
+        const newYPosition = ((now - startOfDay) / 86400000) * img.clientHeight + img.offsetTop-10;
+        console.log(img.offsetTop, img.clientHeight, newYPosition);
         line.style.top = `${newYPosition}px`;
     }
 
     updateLinePosition();
-    setInterval(updateLinePosition, 60000); // Update every minute
+    setInterval(updateLinePosition, 1000); // Update every minute
 
     const timeElement = document.getElementById('current-time');
 
     function updateTime() {
         const now = new Date();
         const options = { timeZone: 'Asia/Seoul', hour12: false };
-        const timeString = now.toLocaleTimeString('ko-KR', options);
+        const timeString = "지금은? " + now.toLocaleTimeString('ko-KR', options);
         timeElement.textContent = timeString;
     }
 
